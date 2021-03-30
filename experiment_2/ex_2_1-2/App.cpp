af11ef731a136615e7993c372175f195d9530688
logan8866 <1977820696@qq.com>
2021-03-30 20:53:31 +0800

    20210330

---
+++ experiment_2/ex_2_1-2/App.cpp
@@ -0,0 +1,11 @@
+#include"Myclass.cpp"
+
+int main(){
+	Myclass obj1();
+	Myclass obj2(5,8);
+	obj1.x = 1;
+	obj1.y = 3;
+	obj1.print();
+	obj2.print();
+	return 0;
+}
---
+++ experiment_2/ex_2_1-2/Myclass.h
@@ -0,0 +1,7 @@
+class Myclass{
+	public:
+		int x,y;
+		Myclass();
+		Myclass(int a,int b);
+		void print();
+};
